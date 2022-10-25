fe841ce64399e8ec5a2ac91d877fa61fbb73b174
nazmussakibofficial <nazmussakibofficial19@gmail.com>
2022-10-25 17:16:43 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = process.env.PORT || 5000
+
+app.use(cors());
+
+const categories = require('./data/categories.json')
+const course = require('./data/course.json')
+
+app.get('/', (req, res) => {
+    res.send('Courses API Running')
+})
+
+app.get('/categories', (req, res) => {
+    res.send(categories)
+})
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '06') {
+        res(course)
+    }
+    else {
+        const category_course = course.filter(n => n.category_id === id);
+        res.send(category_course)
+    }
+
+})
+
+app.get('/course', (req, res) => {
+    res.send(course)
+})
+
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedcourse = course.find(n => n._id === id);
+    res.send(selectedcourse);
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
